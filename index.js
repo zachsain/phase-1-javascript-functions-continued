c76f516b62b2497ff8da5233465bbccd53c0d408
Zachary Sain <zachsain@gmail.com>
2021-11-01 11:57:29 -0500

    completed again

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+function saturdayFun(pass1 = "roller-skate") {
+    return `This Saturday, I want to ${pass1}!`
+}
+
+saturdayFun()
+
+function mondayWork(pass1 = "go to the office") {
+    return `This Monday, I will ${pass1}.`;
+}
+
+function wrapAdjective(p1 = "*") {
+    return function(spec = "special") {
+        return `You are ${p1}${spec}${p1}!`
+    } 
+}
+
+wrapAdjective("*")("not good at this");
